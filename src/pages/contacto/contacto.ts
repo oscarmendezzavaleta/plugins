0c3c33d4bcb764752acf4433af758c81c510c839
oscarmendezzavaleta <oscar.mendez.zavaleta@gmail.com>
2017-11-20 19:19:32 -0500

    sexto cambio con contactos fix done

--- src/pages/contacto/contacto.ts
+++ src/pages/contacto/contacto.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-//import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
+import { Contacts, Contact, ContactField, ContactName } from '@ionic-native/contacts';
 /**
  * Generated class for the ContactoPage page.
  *
@@ -15,16 +15,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ContactoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-//,private contacts: Contacts
-    // let contact: Contact = this.contacts.create();
+  constructor(public navCtrl: NavController, public navParams: NavParams,private contacts: Contacts) {
+
+    let contact: Contact = this.contacts.create();
     
-    // contact.name = new ContactName(null, 'Smith', 'John');
-    // contact.phoneNumbers = [new ContactField('mobile', '6471234567',true)];
-    // contact.save().then(
-    //   () => console.log('Contact saved!', contact),
-    //   (error: any) => console.error('Error saving contact.', error)
-    // );
+    contact.name = new ContactName(null, 'Smith', 'John');
+    contact.phoneNumbers = [new ContactField('mobile', '6471234567',true)];
+    contact.save().then(
+      () => console.log('Contact saved!', contact),
+      (error: any) => console.error('Error saving contact.', error)
+    );
   }
 
   ionViewDidLoad() {
